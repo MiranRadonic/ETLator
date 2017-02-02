f37030a2b1c33e42106fbe9de7ec9d3cdc8621ec
Miran Radonic <Miran@Mirans-MacBook-Pro.local>
2017-02-01 23:17:02 -0800

    v1

---
+++ lib/connector.py
@@ -0,0 +1,147 @@
+class Connector(object):
+    """ Abstract class for different connectors. """
+
+    def copy(self):
+        pass
+
+    def connect(self, connection_params=None):
+        pass
+
+    def cursor(self):
+        if self.conn is None:
+            raise NoConnectionError
+        try:
+            return self.conn.cursor()
+        except:
+            raise ConnectorError
+
+    def commit(self):
+        if self.conn is None:
+            raise NoConnectionError
+        try:
+            self.conn.commit()
+        except:
+            raise ConnectorError("Can't commit")
+
+    def rollback(self):
+        if self.conn is None:
+            raise NoConnectionError
+        try:
+            self.conn.rollback()
+        except:
+            raise ConnectorError("Can't rollback")
+
+    def close(self):
+        if self.conn is None:
+            raise NoConnectionError
+        try:
+            self.conn.close()
+        except:
+            raise ConnectorError("Can't close connection")
+
+
+class ConnectorError(Exception):
+    """ Generic error of the Connector. """
+
+    def __init__(self, arg = None):
+      self.args = [arg]
+
+
+class NoConnectionError(ConnectorError):
+    """ The connection doesn't exist. Use connect() method. """
+
+    def __init__(self, arg = None):
+      self.args = [arg]
+
+
+class PosgresqlConnector(Connector):
+    """ Connector for posgresql database. Uses Psycopg2. """
+
+    def __init__(self, connection_string = None):
+        self.psycopg2 = __import__('psycopg2')
+        if connection_string is not None:
+            self.connection_string = connection_string
+            self.connect(connection_string)
+
+    def copy(self):
+        return PosgresqlConnector(self.connection_string)
+
+    def connect(self, connection_string = None):
+        if connection_string is not None:
+            self.connection_string = connection_string
+        if self.connection_string is None:
+            raise ConnectorError("Connection string is None")
+        try:
+            self.conn = self.psycopg2.connect(self.connection_string)
+        except:
+            raise ConnectorError("Can't connect")
+
+
+class SQLiteConnector(Connector):
+    """ Connector for SQLite database. Uses built-in module sqlite3. """
+
+    def __init__(self, connection_string = None):
+        self.sqlite3 = __import__('sqlite3')
+        if connection_string is not None:
+            self.connection_string = connection_string
+            self.connect(connection_string)
+
+    def copy(self):
+        return SQLiteConnector(self.connection_string)
+
+    def connect(self, connection_string = None):
+        if connection_string is not None:
+            self.connection_string = connection_string
+        if self.connection_string is None:
+            raise ConnectorError("Connection string is None")
+        try:
+            self.conn = self.sqlite3.connect(self.connection_string)
+        except:
+            raise ConnectorError("Can't connect")
+
+
+class MySQLConnector(Connector):
+    """ Connector for MySQL database. Uses mysql. """
+
+    def __init__(self, connection_params = None):
+        self.mysql_connector = __import__('mysql.connector')
+        if connection_params is not None:
+            self.connection_params = connection_params
+            self.connect(connection_params)
+
+    def copy(self):
+        return MySQLConnector(self.connection_params)
+
+    def connect(self, connection_params = None):
+        if connection_params is not None:
+            self.connection_params = connection_params
+        if self.connection_params is None:
+            raise ConnectorError("Connection string is None")
+        try:
+            self.conn = self.mysql_connector.connector.connect(host=self.connection_params['host'], user=self.connection_params['user'], passwd=self.connection_params['passwd'], db=self.connection_params['db'])
+            self.conn.cursor().execute('SET foreign_key_checks = 0')
+        except Exception as e:
+            raise ConnectorError("Can't connect " + e.message)
+
+
+class SQLServerConnector(Connector):
+    """ Connector for Microsoft SQL Server database. Uses pymssql. """
+
+    def __init__(self, connection_params = None):
+        self.mysql_connector = __import__('pymssql')
+        if connection_params is not None:
+            self.connection_params = connection_params
+            self.connect(connection_params)
+
+    def copy(self):
+        return SQLServerConnector(self.connection_params)
+
+    def connect(self, connection_params = None):
+        if connection_params is not None:
+            self.connection_params = connection_params
+        if self.connection_params is None:
+            raise ConnectorError("Connection string is None")
+        try:
+            self.conn = self.mysql_connector.connector.connect(host=self.connection_params['host'], user=self.connection_params['user'], password=self.connection_params['password'], database=self.connection_params['database'])
+        except Exception as e:
+            raise ConnectorError("Can't connect " + e.message)
---
+++ main.py
@@ -0,0 +1,142 @@
+import lib.connector
+import lib.table
+import lib.logging
+import os
+from os import walk
+from multiprocessing import pool, Manager
+from datetime import datetime
+from blockdiag import parser, builder, drawer
+
+# Default. Change in setup if You wish.
+poolsize = 5
+drawdiag = True
+
+manager = Manager()
+counters = manager.dict()
+counters['inserts'] = 0
+counters['updates'] = 0
+counters['warnings'] = 0
+counters['errors'] = 0
+
+paths = manager.dict()
+
+# masterlogger = logging.getLogger('master_logger')
+# masterlogger.basicConfig(
+#     format='%(asctime)s %(message)s'
+#     , filename=os.path.dirname('./log/' + datetime.now().strftime('master_%H_%M_%d_%m_%Y.log')
+#     , level=logging.WARNING)
+
+#print(os.path.dirname('./log/' + datetime.now().strftime('master_%H_%M_%d_%m_%Y.log')))
+
+masterlogger = lib.logging.Logger(
+    name = 'Master'
+    , filename='./Logs/' + datetime.now().strftime('master_%H_%M_%d_%m_%Y.log')
+    , minlevel = 'ERROR')
+
+def exec_flow((filename, flow_dir, counters)):
+    with open('./flow/' + flow_dir + '/' + filename) as source_file:
+        logpath = './flow/' + flow_dir + '/Logs/' + filename + datetime.now().strftime('_%H_%M_%d_%m_%Y.log')
+        tables = []
+        default_args = (logpath, masterlogger, counters, paths, tables)
+        print('Executing ' + filename)
+        exec(source_file.read())
+    for table in tables:
+        try:
+            table.finalize()
+        except:
+            pass
+
+setup_files = []
+for (dirpath, dirnames, filenames) in walk('./setup'):
+    setup_files.extend(filenames)
+    break
+
+for f in setup_files:
+    if f[0] != '.':
+        with open('./setup/' + f) as source_file:
+            exec(source_file.read())
+
+flow_dirs = []
+for (dirpath, dirnames, filenames) in walk('./flow'):
+    flow_dirs.extend(dirnames)
+    break
+
+for flow_dir in flow_dirs:
+    flow_files = []
+    for (dirpath, dirnames, filenames) in walk('./flow/' + flow_dir):
+        flow_files.extend(filenames)
+        break
+
+    args = [(x, flow_dir, counters) for x in flow_files if x[0] != '.']
+    # Parallel
+    try:
+        workerpool = pool.Pool(poolsize)
+        workerpool.map(exec_flow, args)
+    finally:
+        workerpool.close()
+        workerpool.join()
+
+final_files = []
+for (dirpath, dirnames, filenames) in walk('./finalization'):
+    final_files.extend(filenames)
+    break
+
+for f in final_files:
+    if f[0] != '.':
+        with open('./finalization/' + f) as source_file:
+            exec(source_file.read())
+
+# drawing diagram
+if drawdiag:
+    dirs = dict()
+    bdiagsrc = 'blockdiag {\n'
+    for table in paths.keys():
+        files = dict()
+        d = paths[table][0]
+        f = paths[table][1]
+        if f not in files.keys():
+            files[f] = [table]
+        else:
+            files[f].append(table)
+        if d not in dirs.keys():
+            dirs[d] = files
+        else:
+            dirs[d].update(files)
+
+    print dirs
+    prevdir = None
+    for d in sorted(dirs.keys()):
+        bdiagsrc += d + ' [label = "' + d + '", shape = square];\n'
+        dend = d + '_END'
+        bdiagsrc += dend + ' [label = "", shape = minidiamond];'
+        if prevdir is not None:
+            bdiagsrc += prevdir + ' -> ' + d + ';'
+        prevdir = dend
+        for filename in sorted(dirs[d].keys()):
+            bdiagsrc += filename + ' [label = "' + filename + '", shape = roundedbox];\n'
+            bdiagsrc +=  d + ' -> ' + filename + ' -> ' + dend + ';\n'
+            # bdiagsrc +=  d + ' -> ' + filename + ';\n'
+            # prevtable = filename
+            # for table in sorted(dirs[d][filename]):
+            #     bdiagsrc += table + ' [label = "' + table + '"];\n'
+            #     bdiagsrc +=  prevtable + ' -> ' + table + ';\n'
+            #     prevtable = table
+            # bdiagsrc += prevtable + ' -> ' + dend + ';\n'
+    bdiagsrc += '}'
+    tree = parser.parse_string(bdiagsrc)
+    diagram = builder.ScreenNodeBuilder.build(tree)
+    draw = drawer.DiagramDraw('PNG', diagram, filename="diagram.png")
+    draw.draw()
+    draw.save()
+
+masterlogger.finalize()
+
+print('Finished!')
+if counters['inserts'] > 0:
+    print(str(counters['inserts']) + ' inserts')
+if counters['updates'] > 0:
+    print(str(counters['updates']) + ' updates')
+if counters['warnings'] > 0:
+    print(str(counters['warnings']) + ' warnings')
+if counters['errors'] > 0:
+    print(str(counters['errors']) + ' errors')
